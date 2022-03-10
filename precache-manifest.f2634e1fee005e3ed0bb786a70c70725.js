63e46d6ab546116668bd1699d7ecb8da6ed7d6b4
Revaldi Rahmatmulya <revaldiaye@gmail.com>
2022-03-10 14:52:31 +0700

    Updates

---
+++ precache-manifest.f2634e1fee005e3ed0bb786a70c70725.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "13564eb5baa765654b1b",
+    "url": "/revakaryapersada/static/css/main.ee1c9a06.chunk.css"
+  },
+  {
+    "revision": "13564eb5baa765654b1b",
+    "url": "/revakaryapersada/static/js/main.13564eb5.chunk.js"
+  },
+  {
+    "revision": "cd474f2dcda6b2605ecf",
+    "url": "/revakaryapersada/static/js/1.cd474f2d.chunk.js"
+  },
+  {
+    "revision": "8d02d5ca0f2ef8dace78",
+    "url": "/revakaryapersada/static/css/2.0f345be4.chunk.css"
+  },
+  {
+    "revision": "8d02d5ca0f2ef8dace78",
+    "url": "/revakaryapersada/static/js/2.8d02d5ca.chunk.js"
+  },
+  {
+    "revision": "f5f068d560de8b1f57eb",
+    "url": "/revakaryapersada/static/js/runtime~main.f5f068d5.js"
+  },
+  {
+    "revision": "65676f3b45edd0d9c863db476ef6c07f",
+    "url": "/revakaryapersada/static/media/logo.65676f3b.png"
+  },
+  {
+    "revision": "72a15ebf1522607934cc90fd217b6ee1",
+    "url": "/revakaryapersada/static/media/hero-1.72a15ebf.jpg"
+  },
+  {
+    "revision": "32a396536689446a2e68b875411ecc69",
+    "url": "/revakaryapersada/static/media/about-img.32a39653.jpg"
+  },
+  {
+    "revision": "a40611b3cdb5c74cb8ef15b24bd9dd59",
+    "url": "/revakaryapersada/static/media/chart.a40611b3.svg"
+  },
+  {
+    "revision": "32b12f0f898beee7b9d1ce270d8a8ec4",
+    "url": "/revakaryapersada/static/media/contact-img.32b12f0f.jpg"
+  },
+  {
+    "revision": "e26c3c38ce697831bf670b20892621a9",
+    "url": "/revakaryapersada/static/media/visi-img.e26c3c38.jpg"
+  },
+  {
+    "revision": "ff39560e36088e0016a6aa1a7ab2e46c",
+    "url": "/revakaryapersada/static/media/icons8_whatsapp.ff39560e.svg"
+  },
+  {
+    "revision": "41b8f6baa280bf32b471509850c8d05f",
+    "url": "/revakaryapersada/static/media/icons8_Instagram.41b8f6ba.svg"
+  },
+  {
+    "revision": "6215e42d819bcf493fb3461e1d9f43ca",
+    "url": "/revakaryapersada/static/media/hero-2.6215e42d.jpg"
+  },
+  {
+    "revision": "60a14ef7f3e8311d7be3dc09c2412021",
+    "url": "/revakaryapersada/index.html"
+  }
+];
